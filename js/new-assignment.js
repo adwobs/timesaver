6f2469f8def50df0d361fcd9352ef28fb3bbad2d
alvinofori <alvin.ofori@outlook.com>
2016-11-24 00:11:05 +0000

    created teh addNewAssignment function

--- js/new-assignment.js
+++ js/new-assignment.js
@@ -1,3 +1,9 @@
-function(){
-    
+/*
+* function to take new assignment details from the view, and store in database.
+*/
+function addNewAssignment(){
+    //take the values from the input fields, and store in variables.
+
+
+    //use jquery to send the values into the database.
 }
